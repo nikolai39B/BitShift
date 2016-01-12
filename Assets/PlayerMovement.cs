7a05286aa245605b85e382ef9451217e7e412e41
nikolai39B <nikolai39b@gmail.com>
2016-01-11 20:30:24 -0500

    Initial commit.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+    public float maxSpeed = 5.0f;
+
+	void FixedUpdate()
+    {
+        Rigidbody2D currRigidbody = GetComponent<Rigidbody2D>();
+
+        // Move the player
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            currRigidbody.AddForce(new Vector2(0, movementSpeed));
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            currRigidbody.AddForce(new Vector2(0, -movementSpeed));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            currRigidbody.AddForce(new Vector2(-movementSpeed, 0));
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            currRigidbody.AddForce(new Vector2(movementSpeed, 0));
+        }
+
+        Debug.Log(currRigidbody.velocity.magnitude);
+
+        // Restrict the player's max speed
+        if (currRigidbody.velocity.magnitude > maxSpeed)
+        {
+            currRigidbody.velocity = currRigidbody.velocity.normalized * maxSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerEyeManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEyeManager : MonoBehaviour
+{
+    // Eye distance
+    public float distanceFromEyeToFaceCenter = 0.1f;
+    public float DistanceFromEyeToFaceCenter { get { return distanceFromEyeToFaceCenter; } }
+
+    // Player eye instance
+    public GameObject playerEyeIntance;
+
+    // Current camera
+    public Camera currCamera;
+
+	void Start()
+    {
+	
+	}
+	
+	void Update()
+    {
+        // Get the location of ourself and of the mouse
+        Vector3 facePosition = GetComponent<Transform>().position;
+        Vector3 mousePosition = currCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        // Get the direction the eye should look
+        Vector2 eyeDirection = new Vector2(
+            mousePosition.x - facePosition.x,
+            mousePosition.y - facePosition.y);
+        eyeDirection.Normalize();
+        eyeDirection *= distanceFromEyeToFaceCenter;
+
+        // Get the new position for the eye
+        Vector3 eyeLocation = new Vector3(
+            facePosition.x + eyeDirection.x,
+            facePosition.y + eyeDirection.y,
+            playerEyeIntance.transform.position.z);
+        playerEyeIntance.transform.position = eyeLocation;
+	}
+}
